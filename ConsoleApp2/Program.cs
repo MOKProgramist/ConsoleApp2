77a21823bd92b9620ecd29ee323d166aa6f41a72
RudovEgor <egorka.ruda@mail.ru>
2021-10-19 11:57:12 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+
+            Console.Write("Введите x = ");
+            x = Convert.ToDouble(Console.ReadLine()); // считываем с консоли
+
+            if(x < -2) 
+            {
+                y = 2 * Math.Log(x + 2);
+            } else
+            {
+                y = 
+            }
+        }
+    }
+}
